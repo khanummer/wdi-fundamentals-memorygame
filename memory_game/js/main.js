e924d605c39d6e2c931a354607220455e622100a
Ummer Khan <ummernkhangit@gmail.com>
2018-12-06 14:22:51 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,31 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+
 console.log("User flipped" + cardOne);
 console.log("User flipped" + cardTwo);
+
+var cards = [queen, queen, king, king];
+
+var cardsInPlay = [];
+
+var card0ne = cards[0]
+
+cardsInPlay.push('card0ne')
+
+console.log("User flipped Queen")
+
+var cardTwo = cards[2]
+
+cardsInPlay.push('cardTwo')
+
+console.log("User flipped king")
+
+if ('cardsInPlay'.length === 2) {
+	
+}
+else if (cardsInPlay[0] === cardsInPlay[1]) {
+alert("You found a match!")
+}
+else if (cardsInPlay[0] /=== cardsInPlay[1]) {
+	alert("Sorry, try again.")
+}
+
